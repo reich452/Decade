cb1295598409a926b44cd440b74116b0b4757fab
reich452 <reich452@regis.edu>
2017-05-01 16:12:46 -0600

    Updated the image controller

--- Decade/ImageController .swift
+++ Decade/ImageController .swift
@@ -7,7 +7,27 @@
 //
 
 import Foundation
+import UIKit
 
 class ImageController {
     
+    static let baseUrlString = "https://api.cognitive.microsoft.com/bing/v5.0/images/search"
+    
+    static func image(forURL url: String, completion: @escaping (UIImage?) -> Void) {
+        
+        guard let url = URL(string: url) else {
+            fatalError("Image URL optional is nil")
+        }
+        
+        NetworkController.performRequest(for: url, httpMethod: .get) { (data, error) in
+            guard let data = data,
+                let image = UIImage(data: data) else {
+                    
+                    DispatchQueue.main.async { completion(nil) }
+                    return
+            }
+            DispatchQueue.main.async { completion(image) }
+            
+        }
+    }
 }
