ce9e45b36c38a068b2bfbff136cff6a70ca6f334
reich452 <reich452@regis.edu>
2017-05-03 14:53:03 -0600

    Updated the decadeSearchConteroller, correct dispatchGroup

--- Decade/DecadeSearchController.swift
+++ Decade/DecadeSearchController.swift
@@ -19,13 +19,13 @@ class DecadeSearchController {
     private let contentURLKey = "contentUrl"
     private let apiParameter = "Ocp-Apim-Subscription-Key"
     private let baseURL = URL(string: "https://api.cognitive.microsoft.com/bing/v5.0/images/search")
-    private let apiKey = "c3f846e0028943f5afd96e9cdd6eae73"
+    private let apiKey = "0231de06566d4717873444afb447e586"
     
     func searchForImagesWith(searchTerm: String, completion: @escaping ([Decade]?, DecadeError?) -> Void) {
         guard let baseURL = baseURL else { completion([], .baseUrlFailed); return }
-        let urlParameters = ["q": searchTerm, apiParameter: apiKey, countKey: "200"]
+        let urlParameters = ["q": searchTerm]
         
-        NetworkController.performRequest(for: baseURL, httpMethod: .get, urlParameters: urlParameters, body: nil) { (data, error) in
+        NetworkController.performRequest(for: baseURL, apiKey: apiKey, httpMethod: .get, urlParameters: urlParameters, body: nil) { (data, error) in
             if let error = error { print("Error: searching for image \(error.localizedDescription)")
                 completion([], .imageSearchFailure); return }
             
@@ -33,14 +33,23 @@ class DecadeSearchController {
                 let jsonDictionaries = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String: Any],
                 let imageArray = jsonDictionaries["value"] as? [[String: Any]] else { completion([], .jsonConversionFailure); return }
             
-            let images = imageArray.flatMap( {Decade(jsonDictionary: $0)})
+            let decades = imageArray.flatMap( {Decade(jsonDictionary: $0)})
             
-            for image in images {
-                ImageController.image(forURL: image.contentUrlString, completion: { (newImage) in
-                    
+            let group = DispatchGroup()
+            
+            for decade in decades {
+                group.enter()
+                ImageController.image(forURL: decade.contentUrlString, completion: { (newImage) in
+                    print("Wheres da image")
+                    decade.decadeImage = newImage
+                    group.leave()
                 })
             }
-            completion(images, nil)
+            
+            group.notify(queue: DispatchQueue.main, execute: { 
+                
+                completion(decades, nil)
+            })
         }
     }
 }
